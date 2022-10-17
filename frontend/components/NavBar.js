df29186fe9f001216407afaf3c96bebb7a5a00bb
julydev144hz <ippolito.julianet36@gmail.com>
2022-10-17 14:33:08 -0300

    navbar v2

--- frontend/components/NavBar.js
+++ frontend/components/NavBar.js
@@ -5,15 +5,15 @@ function NavBar() {
         <nav className={styles.navbar}>
                 <ul className={styles.ul}>
                     <li className={styles.li_icon}>
-                        <img src="favicon.png" alt="icon" className={styles.icon}/>
+                        <img src="icon.png" alt="icon" className={styles.icon}/>
                     </li>
                     <li className={styles.li_search}>
                         <input type="text" className={styles.li_search_input} />
                         <img src="search.svg" alt="search" className={styles.li_search_img}/>
                     </li>
                     <li className={styles.li}>
-                        <span>Julian Ippolito</span>
-                        <img src="" alt="perfil" />
+                        <span className={styles.li_user_name}>Julian Ippolito</span>
+                        <img src="" alt="perfil" className={styles.li_user_img} />
                     </li>
                 </ul>
         </nav>
