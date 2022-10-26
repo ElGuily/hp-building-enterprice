89085dff4f56b39449dcabee3dc989daaa357190
TomasFernandez1 <fernandez.tomaset36@gmail.com>
2022-10-26 09:33:50 -0300

    Añadido clase PCs y PC_Oficina

---
+++ backend/main/src/main/PC_Oficina.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author ET36
+ */
+public class PC_Oficina extends PCs {
+    
+    private boolean all_in_one;
+
+    public PC_Oficina(boolean all_in_one, int id_pc, double valor_PC, int componentes_imp) {
+        super(id_pc, valor_PC);
+        this.all_in_one = all_in_one;
+    }
+    
+    @Override
+    public double calcularPrecio() {
+        if(this.all_in_one){
+            this.valor_PC+=5000;
+        }
+        return this.valor_PC;
+    }
+    
+}
