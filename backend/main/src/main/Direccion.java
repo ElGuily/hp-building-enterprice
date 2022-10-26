ab7c4ba28a6ec50d75d07619ed9305a88cee3075
facunare <arechaga.facundoet36@gmail.com>
2022-10-26 08:23:37 -0300

    backend java

---
+++ backend/main/src/main/Direccion.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author ET36
+ */
+public class Direccion {
+    private String calle;
+    private int 
+}
