cde81d0759fccc3c1fd1814a6815b02dc0af300e
facunare <arechaga.facundoet36@gmail.com>
2022-10-26 10:41:06 -0300

    2 clases

---
+++ backend/main/src/main/PC_Diseño.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author ET36
+ */
+public class PC_Diseño extends PCs {
+
+    public PC_Diseño(int id_pc, double valor_pc) {
+        super(id_pc, valor_pc);
+        
+    }
+    
+    public double calcularPrecio(){
+        return 0;
+    }
+    
+    
+}
